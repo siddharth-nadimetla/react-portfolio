a7c4e36e72c5ac6d66a1d15debef40466e3d01e7
Siddharth Nadimetla <158156363+siddharth-nadimetla@users.noreply.github.com>
2024-04-25 23:09:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-portfolio/",
+  base: "https://react-portfolio-phi-woad.vercel.app/",
   plugins: [react()],
   css: {
     modules: {
